e36b779509c108113905a94677be4b298bc5eb22
Sara Lissette <lissette.ibnz@gmail.com>
2020-03-29 17:38:31 +0100

    Create a ErrorBoundary component

---
+++ src/common/components/error-boundary/error-boundary.component.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<{}, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Actualiza el estado para que el siguiente renderizado muestre la interfaz de repuesto
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // También puedes registrar el error en un servicio de reporte de errores
+    // logErrorToMyService(error, errorInfo);
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Puedes renderizar cualquier interfaz de repuesto
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
