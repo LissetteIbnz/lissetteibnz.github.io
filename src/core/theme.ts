23009e81238a5b055d77dcf129e97aadd5f0aa26
Sara Lissette <lissette.ibnz@gmail.com>
2020-04-08 09:10:57 +0100

    Create a theme

---
+++ src/core/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  text: {
+    primary: 'rgba(0, 0, 0, 0.87)',
+    secondary: 'rgba(0, 0, 0, 0.54)',
+    oposite: '#f9f9f9',
+    disabled: 'rgba(0, 0, 0, 0.38)',
+    highlight: '#b665e6',
+  },
+} as const;
