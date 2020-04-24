ad534f3e3883914c8076f4d514b6d98be8f8e75f
Sara Lissette <lissette.ibnz@gmail.com>
2020-04-24 18:52:14 +0100

    Update config with environment variables

--- src/core/config.ts
+++ src/core/config.ts
@@ -5,12 +5,9 @@ export const config = {
   githubConvertedToken: process.env.GITHUB_CONVERTED_TOKEN,
   githubUserName: 'LissetteIbnz',
   githubRepo: 'lissetteibnz.github.io',
-
-  // Set it to true if you have a Github app to add to this project
-  // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: process.env.OAUTH_CLIENT_ID,
+  OAuthSecret: process.env.OAUTH_SECRET,
 
   // Settings
   tagFilterBlog: 'blog',
